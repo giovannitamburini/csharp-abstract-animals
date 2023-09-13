0023ae8de5f391fb24b289501cbd9f57c876f1ab
giovannitamburini <giovatambu@gmail.com>
2023-09-13 12:59:58 +0200

    added exercise delivery in a comment

--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace csharp_abstract_animals
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace csharp_abstract_animals
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+            Esercizio 1 : classi astratte
+
+            Dobbiamo realizzare un programma che rappresenti il regno animale.
+
+            Creiamo quindi una classe astratta Animale e delle classi che la estendono : Cane, Passerotto, Aquila, Delfino
+
+            Vogliamo che gli animali abbiano i seguenti metodi
+            - void Dormi() (mostra a video “Zzz”)
+            - void Verso() (mostra a video il verso fatto dall'animale specifico)
+            - void CosaMangi() (mostra a video quello che mangia: erba, carne, ...?)
+
+            Quali di questi metodi devono essere implementati nella classe astratta e quali devono essere lasciati astratti e
+            implementati dalle classi che la estendono?
+
+            Una volta create le classi, nel programma istanziare alcuni animali e utilizzare i metodi implementati.
+            */
         }
     }
 }
